c48899933b9c800f047113fd3b3ea685a9b54a72
Arlo Mac_Feely <macfeelyarlo2@gmail.com>
2026-08-01 13:29:34 +0100

    made a random triangle test script

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Numerics;
 using System.Drawing;
 using Graphics_Library.Rendering;
+using Graphics_Library.Objects;
 
 namespace Graphics_Library
 {
@@ -9,6 +10,7 @@ namespace Graphics_Library
         const int Length = 2000;
         static void Main()
         {
+            Random random = new Random();
 
             Canvas canvas = new Canvas(800, 600, "mycanvas");
 
@@ -17,9 +19,12 @@ namespace Graphics_Library
             while (running)
             {
                 running = !canvas.CheckQuit();
-
-                Objects.Triangle testtriangle = new Objects.Triangle(new Vector2(100,100),new Vector2(300,600),new Vector2(500,500),Color.FromArgb(255,100,100));
-                canvas = Rasterizer.DrawTriangle(testtriangle,canvas);
+                Vector2 p1 = new Vector2(random.Next(0,801),random.Next(0,601));
+                Vector2 p2 = new Vector2(random.Next(0,801),random.Next(0,601));
+                Vector2 p3 = new Vector2(random.Next(0,801),random.Next(0,601));
+                Color colour = Color.FromArgb(random.Next(0,256),random.Next(0,256),random.Next(0,256));
+                Triangle triangle = new Triangle(p1,p2,p3,colour);
+                canvas = Rasterizer.DrawTriangle(triangle, canvas);
                 
                 canvas.UpdateCanvas();
                 frame += 1;
